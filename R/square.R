6e2decc7139eb75cf80c52245f71401f863bd1e7
Shuchen <wusc@umich.edu>
2025-11-04 15:48:52 -0500

    include vig

---
+++ R/square.R
@@ -0,0 +1,16 @@
+#'Square
+#'
+#'
+#'
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+#'Gets the square of a number
+#'@param x input value
+#'@return the square of x
+square = function(x) {
+  return (x^2)
+}
